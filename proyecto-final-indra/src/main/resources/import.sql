bbe8935801f2a88349f33d2ecd0e886589765a5e
Edwin Rincon <rinconedwin@hotmail.com>
2022-02-17 13:01:59 +0100

    Clientes.html fix

--- proyecto-final-indra/src/main/resources/import.sql
+++ proyecto-final-indra/src/main/resources/import.sql
@@ -1,9 +1,9 @@
 -- clientes
-INSERT INTO empleados(nombre,apellido,sexo,telefono) VALUES ("Jose","Garcia","hombre","642775959");
-INSERT INTO empleados(nombre,apellido,sexo,telefono) VALUES ("Maria","Perez","mujer","642775958");
-INSERT INTO empleados(nombre,apellido,sexo,telefono) VALUES ("David","Orue","hombre","642775957");
-INSERT INTO empleados(nombre,apellido,sexo,telefono) VALUES ("Sergio","Gomez","hombre","642775956");
-INSERT INTO empleados(nombre,apellido,sexo,telefono) VALUES ("Antonio","Cardona","mujer","642775955");
+INSERT INTO clientes(nombre,apellidos,sexo,telefono) VALUES ("Jose","Garcia","hombre","642775959");
+INSERT INTO clientes(nombre,apellidos,sexo,telefono) VALUES ("Maria","Perez","mujer","642775958");
+INSERT INTO clientes(nombre,apellidos,sexo,telefono) VALUES ("David","Orue","hombre","642775957");
+INSERT INTO clientes(nombre,apellidos,sexo,telefono) VALUES ("Sergio","Gomez","hombre","642775956");
+INSERT INTO clientes(nombre,apellidos,sexo,telefono) VALUES ("Antonio","Cardona","mujer","642775955");
 
 
 -- productos
