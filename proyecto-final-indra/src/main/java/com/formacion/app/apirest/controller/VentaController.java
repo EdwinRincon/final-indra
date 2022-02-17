2ac74ec512a6fa73604dd4f3445c5e38e78d0114
josep <garciajose489@gmail.com>
2022-02-17 13:55:53 +0100

    ventacontroller sin imports sobrantes

--- proyecto-final-indra/src/main/java/com/formacion/app/apirest/controller/VentaController.java
+++ proyecto-final-indra/src/main/java/com/formacion/app/apirest/controller/VentaController.java
@@ -4,10 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import com.formacion.app.apirest.entity.Venta;
 import com.formacion.app.apirest.service.VentaService;
 
 @Controller
