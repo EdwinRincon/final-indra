4fd8da41339429d671f6ceda9e0c8e6a49edb6ea
Edwin Rincon <rinconedwin@hotmail.com>
2022-02-17 11:52:51 +0100

    configuracion inicial

---
+++ proyecto-final-indra/src/main/resources/import.sql
@@ -0,0 +1,12 @@
+-- clientes
+INSERT INTO empleados(nombre,apellido,sexo,telefono) VALUES ("Jose","Garcia","hombre","642775959");
+INSERT INTO empleados(nombre,apellido,sexo,telefono) VALUES ("Maria","Perez","mujer","642775958");
+INSERT INTO empleados(nombre,apellido,sexo,telefono) VALUES ("David","Orue","hombre","642775957");
+INSERT INTO empleados(nombre,apellido,sexo,telefono) VALUES ("Sergio","Gomez","hombre","642775956");
+INSERT INTO empleados(nombre,apellido,sexo,telefono) VALUES ("Antonio","Cardona","mujer","642775955");
+
+
+-- productos
+
+
+-- ventas
