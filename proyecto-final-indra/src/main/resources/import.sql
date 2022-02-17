1fd185d3f174cf231749a747074787473a45e842
MissStrange <esanchezloarte@gmail.com>
2022-02-17 13:54:40 +0100

    import.sql
    nuevos campos

--- proyecto-final-indra/src/main/resources/import.sql
+++ proyecto-final-indra/src/main/resources/import.sql
@@ -7,6 +7,14 @@ INSERT INTO clientes(nombre,apellidos,sexo,telefono) VALUES ("Antonio","Cardona"
 
 
 -- productos
+insert into productos (nombre, descripcion, p_unitario, existencias) values ("Televisión", "Una tele", 250, 20);
+insert into productos (nombre, descripcion, p_unitario, existencias) values ("Portatil", "Un ordenador", 890, 25);
+insert into productos (nombre, descripcion, p_unitario, existencias) values ("Altavoces", "Un par de altavoces", 200, 40);
+insert into productos (nombre, descripcion, p_unitario, existencias) values ("Disco duro", "Un disco duro", 150, 10);
 
 
 -- ventas
+insert into ventas(producto_id, cliente_id, cantidad, subtotal, iva, total) values (1, 1, 2, 200, 50, 250);
+insert into ventas(producto_id, cliente_id, cantidad, subtotal, iva, total) values (1, 2, 2, 200, 50, 250);
+insert into ventas(producto_id, cliente_id, cantidad, subtotal, iva, total) values (2, 3, 3, 200, 50, 250);
+insert into ventas(producto_id, cliente_id, cantidad, subtotal, iva, total) values (3, 2, 2, 200, 50, 250);
